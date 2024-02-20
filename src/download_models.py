e3d42fbbef8ec561c94f770bc44459ec19bfe15a
ElmorenohWTF <62709646+ElmorenohWTF@users.noreply.github.com>
2024-02-20 19:27:32 +0100

    Update download_models.py

--- src/download_models.py
+++ src/download_models.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import requests
 
 MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/'
-RVC_DOWNLOAD_LINK = 'https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/'
+RVC_DOWNLOAD_LINK = 'https://huggingface.co/IAHispano/Applio/tree/main/Resources'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 mdxnet_models_dir = BASE_DIR / 'mdxnet_models'
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         print(f'Downloading {model}...')
         dl_model(MDX_DOWNLOAD_LINK, model, mdxnet_models_dir)
 
-    rvc_model_names = ['hubert_base.pt', 'rmvpe.pt']
+    rvc_model_names = ['hubert_base.pt', 'rmvpe.pt' 'fcpe.pt']
     for model in rvc_model_names:
         print(f'Downloading {model}...')
         dl_model(RVC_DOWNLOAD_LINK, model, rvc_models_dir)
